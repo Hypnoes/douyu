7ed45c1040ebefb3fdabf17433cc7f37e33f3314
hypnoes <hypnoes@qq.com>
2018-01-25 22:50:37 +0800

    py/test.py: change output for better understanding.

--- py/test.py
+++ py/test.py
@@ -7,11 +7,11 @@ print()
 
 for i in range(256):
     if i % 8 != 7:
-        print('|', c.color('Color #{0}'.format(i).ljust(11), fg=i), end='')
+        print(c.color('Color #{0}'.format(i).ljust(10), fg=i), end='|')
     else:
-        print('|', c.color('Color #{0}'.format(i).ljust(11), fg=i), end='|\n')
-
+        print(c.color('Color #{0}'.format(i).ljust(10), fg=i), end='\n')
 print()
+
 print(c.color('orange on gray', 'orange', 'gray'))
 print(c.color('nice color', 'white', '#8a2be2'))
 
@@ -25,18 +25,18 @@ print(c.green('green on black', bg='black', style='underline'))
 print(c.red('very important', style='bold+underline'))
 
 
-important = partial(c.color, fg='red', style='bold+underline')
+important = partial(c.color, fg='red', style='bold+negative+blink')
 print(important('this is very important!'))
+print()
 
-print("\n-----------\n")
-print(c.color("gYw", style="none"))
-print(c.color("gYw", style="bold"))
-print(c.color("gYw", style="faint"))
-print(c.color("gYw", style="italic"))
-print(c.color("gYw", style="underline"))
-print(c.color("gYw", style="blink"))
-print(c.color("gYw", style="blink2"))
-print(c.color("gYw", style="negative"))
-print(c.color("gYw", style="concealed"))
-print(c.color("gYw", style="crossed"))
+print(c.color("none", style="none"))
+print(c.color("bold", style="bold"))
+print(c.color("faint", style="faint"))
+print(c.color("italic", style="italic"))
+print(c.color("underline", style="underline"))
+print(c.color("blink", style="blink"))
+print(c.color("blink2", style="blink2"))
+print(c.color("negative", style="negative"))
+print(c.color("concealed", style="concealed"))
+print(c.color("crossed", style="crossed"))
 print()
